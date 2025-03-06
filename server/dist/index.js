7e78c59d31b819b5b7f12125a6d6a8033ad189af
Navdeep Ahir <navdeepahir19@gmail.com>
2025-03-06 21:28:34 +0530

    gitignore changes after removing node_modules

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = require("./config/db");
 const todoRouter_1 = __importDefault(require("./routes/todoRouter"));
@@ -13,10 +12,8 @@ const authRouter_1 = __importDefault(require("./routes/authRouter"));
 dotenv_1.default.config();
 (0, db_1.connectDB)();
 const app = (0, express_1.default)();
-// Middleware for parsing JSON
-app.use(body_parser_1.default.json({ limit: '30mb' }));
-app.use(body_parser_1.default.urlencoded({ limit: '30mb' }));
 app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 // Register the routes
 app.use('/', authRouter_1.default);
 app.use('/', todoRouter_1.default);
